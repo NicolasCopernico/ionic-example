2947db9fa0fe1082c21685da1847103e1b60df59
Carlos <c.bastos100@gmail.com>
2025-08-07 20:13:28 -0300

    Episodio 44 - Estructura y Layout en Ionic

---
+++ src/App copy 2.vue
@@ -0,0 +1,29 @@
+<template>
+  <ion-app>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Basic Structure</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <h1>Hello world</h1>
+    </ion-content>
+    <ion-footer>
+      <ion-toolbar>
+      <p>App Example 2025</p>
+    </ion-toolbar>
+    </ion-footer>
+  </ion-app>
+</template>
+
+<script setup lang="ts">
+import {
+  IonApp, IonContent, IonHeader, IonFooter} from '@ionic/vue';
+
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/App copy 4.vue
@@ -0,0 +1,29 @@
+<template>
+  <ion-menu content-id="main-content">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Menu Example</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">Hello world</ion-content>
+
+  </ion-menu>
+  <ion-page id="main-content">
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button></ion-menu-button>
+        </ion-buttons>
+        <ion-title>Menu</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      Tap to open
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts" setup>
+import { IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+
+</script>
